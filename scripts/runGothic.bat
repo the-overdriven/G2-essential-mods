1d279c714cb4e7421a9fbb6449dac6c39424ac00
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-10-03 22:52:01 +0200

    Add files via upload

---
+++ scripts/runGothic.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Run the file selection script
+call "C:\Program Files (x86)\Steam\steamapps\common\Gothic II\system\GD3D11\textures\SkyDayRandom.bat"
+
+:: Launch the game
+start "" "steam://rungameid/39510"
